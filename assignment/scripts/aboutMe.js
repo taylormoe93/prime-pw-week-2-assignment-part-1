efaab5d63871282df11cf44eb29da784b538761d
Taylor Moe <taylormoe93@gmail.com>
2022-06-19 22:33:20 -0500

    Complete up to 17

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -38,7 +38,7 @@ let food = 'pho';
 console.log('I could always eat ' + food);
 
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
-let pets = 4;
+let pets = 1;
 console.log(pets);
 
 // 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
@@ -94,11 +94,39 @@ else if (pets > allowedPets) {
 // be sure to think through all the scenarios.
 // console.log `mostPets` after the conditional has run.
 
+let mostPets; // Declare undefined variable mostPets
+
+if (pets > friendsPets) {
+	mostPets = pets;
+	console.log(mostPets);
+} // If pets is greater than friendPets then mostPets value equals pets value. Console log the resulting value of mostPets.
+else {
+	mostPets = friendsPets;
+	console.log(mostPets);
+} // If pets value is not greater than friendsPets value, then mostPets equals the value of friendsPets. Console log the resulting value of mostPets.
+
 // 17 - Write a *switch* statement that logs:
 //      "First is the worst" if your lucky number is 1
 //      "Second is the best" if your lucky number is 2
 //      "Third is the one with the polka dot dress" if your lucky number is 3
 //      Otherwise, log "Luck is what happens when preparation meets opportunity"
 //      You'll need to research how to use switch statements!
+
+luckyNumber = 3;
+switch (luckyNumber) {
+	case 1:
+		console.log('First is the worst');
+		break;
+	case 2:
+		console.log('Second is the best');
+		break;
+	case 3:
+		console.log('Third is the one with the polka dot dress');
+		break;
+
+	default:
+		console.log('Luck is what happens when preparation meets opportunity');
+		break;
+}
 
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
